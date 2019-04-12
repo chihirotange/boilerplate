02251e0fcc99cd891586446aca4c64e9008c9a8b
chiichan <ohanasakuiroha@gmail.com>
2019-04-12 10:17:47 +0700

    added imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const server = require('browser-sync').create();
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+
 //BROWSER
 // ====================================
 function serve(done) {
@@ -30,12 +33,29 @@ function watchTask() {
 	watch("./src/sass/**/*.scss", function() {
 		return gulp.src("./src/sass/**/*.scss")
 			.pipe(sass().on('error', sass.logError))
+			.pipe(autoprefixer({
+				browsers: ['last 10 versions'],
+				cascade: false
+			}))
 			.pipe(postcss([cssnano()]))
 			.pipe(gulp.dest('./dist/css'))
 			.pipe(server.stream());
-	});
+	})
 };
+
+// IMAGEMIN
+// =====================================
+function imageMin() {
+	return gulp.src('./src/img/*')
+		.pipe(imagemin([
+			imagemin.optipng({
+				optimizationLevel: 5
+			})
+		]))
+		.pipe(gulp.dest('./dist/img'));
+}
 
 
 // EXPORTS
 exports.default = series(serve, watchTask);
+exports.imagemin = series(imageMin);
