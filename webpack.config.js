0a53bf40ba6180dd60e94778b5bbd14553617248
Chii chan <ohanasakuiroha@gmail.com>
2019-08-04 13:42:10 +0700

    minor configurations

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: "./src/js/main.js",
     output: {
         // change this name V
-        filename: "sumi_verification_bundle.js",
+        filename: "scripts_bundle.js",
         path: path.resolve("./dist/js")
     },
     module: {
